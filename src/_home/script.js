afdfceb8cb5c366b27e71cedfb8021963bcf8422
Jonathan E <115096710+jonathandedev@users.noreply.github.com>
2023-08-05 00:34:32 +0100

    Redesign the write a message page
    I have reimplemented the write a message page with the updated designs.

--- src/_home/script.js
+++ src/_home/script.js
@@ -1,34 +1,79 @@
 "use strict";
 
 function bannerSetup() {
-  const banner1 = document.getElementById("banner-1");
-  const banner2 = document.getElementById("banner-2");
-  const banner3 = document.getElementById("banner-3");
-  let currentBanner = banner1;
+  const banners = document.querySelectorAll("#banner article");
+  const buttons = document.querySelectorAll("#banner-control btn");
+  let currentBanner = 0;
 
-  setInterval(showNextBanner, 5000);
+  let loop = setInterval(showNextBanner, 5000);
 
   function showNextBanner() {
-    nextBanner(currentBanner).classList.add("active");
-    nextBanner(nextBanner(currentBanner)).classList.remove("active");
-    currentBanner.style.zIndex = 3;
-    nextBanner(currentBanner).style.zIndex = 4;
-    setTimeout(prepareBanner, 2000);
+    banners[currentBanner].classList.add("hidden");
+    banners[currentBanner].classList.remove("active");
+    buttons[currentBanner].classList.remove("highlight");
+
+    currentBanner = nextBanner();
+
+    banners[currentBanner].classList.add("animated");
+    banners[currentBanner].classList.add("active");
+    buttons[currentBanner].classList.add("highlight");
+
+    setTimeout(function() {
+      banners[previousBanner()].classList.remove("animated");
+      banners[previousBanner()].classList.remove("hidden");
+      banners[previousBanner()].classList.remove("active");
+    }, 500);
   }
 
-  function prepareBanner() {
-    nextBanner(nextBanner(currentBanner)).style.zIndex = 5;
-    currentBanner = nextBanner(currentBanner);
+  for (let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener("click", function() {
+      const index = Array.prototype.indexOf.call(buttons, this);
+      if (index === currentBanner) {
+        return;
+      }
+
+      clearInterval(loop);
+      banners.forEach(function(banner) {
+        banner.classList.remove("hidden");
+        if (!banner.classList.contains("active")) {
+          banner.classList.remove("animated");
+        }
+      });
+      banners[currentBanner].classList.add("animated");
+      banners[currentBanner].classList.add("hidden");
+      buttons[currentBanner].classList.remove("highlight");
+
+      banners[index].classList.add("animated");
+      banners[index].classList.add("active");
+      buttons[index].classList.add("highlight");
+
+      currentBanner = index;
+      
+      setTimeout(function() {
+        banners[previousBanner()].classList.remove("animated");
+        banners[previousBanner()].classList.remove("hidden");
+        banners[previousBanner()].classList.remove("active");
+        banners[nextBanner()].classList.remove("animated");
+        banners[nextBanner()].classList.remove("hidden");
+        banners[nextBanner()].classList.remove("active");
+      }, 500);
+
+      loop = setInterval(showNextBanner, 5000);
+    });
   }
 
-  function nextBanner(banner) {
-    if (banner == banner1) {
-      return banner2;
-    } else if (banner == banner2) {
-      return banner3;
-    } else {
-      return banner1;
+  function nextBanner() {
+    if (currentBanner === banners.length - 1) {
+      return 0;
     }
+    return currentBanner + 1;
+  }
+
+  function previousBanner() {
+    if (currentBanner === 0) {
+      return banners.length - 1;
+    }
+    return currentBanner - 1;
   }
 }
 
--- src/_write-a-message/script.js
+++ src/_write-a-message/script.js
@@ -77,4 +77,82 @@ function setupForm() {
   }
 }
 
-document.body.onload = setupForm();
+/**
+ * Setup the image carousel for mobile devices.
+ */
+function setupMobileCarousel() {
+  const images = document.querySelectorAll("#write-a-message .mobile-element picture");
+  let currentImage = 0;
+
+  setInterval(showNextImage, 4000);
+
+  function showNextImage() {
+    images[nextImage(currentImage)].classList.add("active");
+    images[nextImage(nextImage(currentImage))].classList.remove("active");
+    images[nextImage(currentImage)].style.zIndex = 2;
+    setTimeout(prepareImage, 2000);
+  }
+
+  function prepareImage() {
+    images[currentImage].style.zIndex = -1;
+    images[nextImage(currentImage)].style.zIndex = 1;
+    images[nextImage(nextImage(currentImage))].style.zIndex = 3;
+    currentImage = nextImage(currentImage);
+  }
+
+  function nextImage(current) {
+    if (current === images.length - 1) {
+      return 0;
+    } 
+    return current + 1;
+  }
+}
+
+function setupDesktopCarousel() {
+
+  let images1 = {
+    list: document.querySelectorAll("#container-1 picture"),
+    current: 0,
+  };
+  let images2 = {
+    list: document.querySelectorAll("#container-2 picture"),
+    current: 0,
+  };
+  let images3 = {
+    list: document.querySelectorAll("#container-3 picture"),
+    current: 0,
+  };
+
+  setInterval(showNextImage, 7000, images2);
+  setTimeout(() => {setInterval(showNextImage, 7000, images1)}, 950);
+  setTimeout(() => {setInterval(showNextImage, 7000, images3)}, 2200);
+
+  function showNextImage(images) {
+    images.list[nextImage(images.current, images.list)].classList.add("active");
+    images.list[nextImage(nextImage(images.current, images.list), images.list)].classList.remove("active");
+    images.list[nextImage(images.current, images.list)].style.zIndex = 2;
+    setTimeout(prepareImage, 2000, images);
+  }
+
+  function prepareImage(images) {
+    images.list[images.current].style.zIndex = -1;
+    images.list[nextImage(images.current, images.list)].style.zIndex = 1;
+    images.list[nextImage(nextImage(images.current, images.list), images.list)].style.zIndex = 3;
+    images.current = nextImage(images.current, images.list);
+  }
+
+  function nextImage(current, list) {
+    if (current === list.length - 1) {
+      return 0;
+    }
+    return current + 1;
+  }
+}
+
+function pageSetup() {
+  setupForm();
+  setupMobileCarousel();
+  setupDesktopCarousel();
+}
+
+document.body.onload = pageSetup;
